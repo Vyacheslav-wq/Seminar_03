dc1d05c117e4c6eaca06739b1366c307c63a81e1
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-05-23 18:10:46 +0300

    Разворот Массива в обратном порядке.

---
+++ Revehs_Arrey/Program.cs
@@ -0,0 +1,14 @@
+﻿//РАЗВОРОТ МАССИВА В ОБРАТНОМ ПОРЯДКЕ.
+Console.Clear();
+Console.Write("Наш массив: ");
+int[] myArrey = { 8, 2, 5, 9, 3, 5, 7 };
+for (int i = 0; i < 7; i++)
+{
+    Console.Write($"{myArrey[i]}, ");
+}
+Console.WriteLine();
+Console.Write("А это массив в обратном порядке: ");
+for (int i = 7 - 1; i >= 0; i--)
+{
+    Console.Write($"{myArrey[i]}, ");
+}
